8448c8672cb6f41eae528e681499bf1bef4bf31a
mousuke <mousuke@users.noreply.github.com>
2025-07-14 07:51:46 +0000

    chatbot up

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request
 import joblib
+from groq import Groq
+import os
+os.environ['GROQ_API_KEY'] = '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'
+# for cloud...
 
 app = Flask(__name__)
+client = Groq()
 
 @app.route("/",methods=["GET","POST"])
 def index():
@@ -17,56 +22,39 @@ def main():
 def dbs():
     return(render_template("dbs.html"))
 
+@app.route("/llama",methods=["GET","POST"])
+def llama():
+    return(render_template("llama.html"))
+
 @app.route("/prediction",methods=["GET","POST"])
 def prediction():
     q = float(request.form.get("q"))
+
     # load model
     model = joblib.load("dbs.jl")
+
     # make prediction
     pred = model.predict([[q]])
-    return(render_template("prediction.html",r=pred))
-
-if __name__ == "__main__":
-    app.run()
-
-#app.py
-#from flask import Flask,render_template,request
-#import joblib
-
-#app = Flask(__name__)
-
-##@app.route("/main",methods=["GET","POST"])
-#def main():
-#    q= request.form.get("q")
-#    # db insert
-#    return(render_template("main.html"))
-
-#@app.route("/dbs",methods=["GET","POST"])
-#def dbs():
-#    q= request.form.get("q")
-    # db insert
-#    return(render_template("dbs.html"))
-
-#@app.route("/",methods=["GET","POST"])
-#def index():
-#    return(render_template("index.html"))
-
-#@app.route("/prediction",methods=["GET","POST"])
-#def prediction():
-#    q = float(request.form.get("q"))
-
-    # load model
-#    model = joblib.load("dbs.jl")
-
-    # make prediction
-#    pred = model.predict([[q]])
 
 #    return(render_template("prediction.html",r=pred))
 
-#@app.route("/prediction",methods=["GET","POST"])
-#def prediction():
-#    q = float(request.form.get("q"))
-#    return(render_template("prediction.html", r=(-50.6*q)+90.2))
+    return(render_template("prediction.html", r=(-50.6*q)+90.2))
 
-#if __name__ == "__main__":
-#    app.run()
+@app.route("/llama_reply",methods=["GET","POST"])
+def llama_reply():
+    q = request.form.get("q")
+
+    # load model
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role": "user",
+                "content": q
+            }
+        ]
+    )
+    return(render_template("llama_reply.html", r=completion.choices[0].message.content))
+
+if __name__ == "__main__":
+    app.run()
